431b5bb3ad7638c70337309ca6ef9244d5b806b4
Raphael Duarte <raphael-duarte@outlook.com>
2024-06-21 05:25:43 -0300

    Adicionar arquivos de projeto.

---
+++ HealthClinical.Services.Api/Controllers/PacientesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthClinical.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacientesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+        [HttpGet("{page/limit}")]
+        public IActionResult GetAll(int page, int limit)
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
